16878d2338711c75183f3c30a4e3c903e0d5acb0
Immanuel Mruk <immanuelmruk@gmail.com>
2024-11-10 22:57:30 +0100

    Add crypt key generation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use crate::config::Settings;
 use structopt::StructOpt;
+use rand::Rng;
+use std::fs::File;
+use std::io::{self, Write};
 mod config;
 mod backup;
 mod encryption;
@@ -10,6 +14,7 @@ enum Command {
     List,
     Backup,
     Restore { filename: String },
+    GenerateKey,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -27,6 +32,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         Command::Restore { filename } => {
             ssh::restore_backup(&filename, &settings)?;
         }
+        Command::GenerateKey => {
+            generate_key(&settings)?;
+        }
     }
+    Ok(())
+}
+
+fn generate_key(settings: &Settings) -> io::Result<()> {
+    let key_path = &settings.encryption_key_path;
+    let mut key_file = File::create(key_path)?;
+
+    let mut rng = rand::thread_rng();
+    let key: Vec<u8> = (0..32).map(|_| rng.gen()).collect();
+
+    key_file.write_all(&key)?;
+    println!("Encryption key successfully generated and saved to {}", key_path.display());
     Ok(())
 }
